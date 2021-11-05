2bbac04e4e17da8c2f4638c7fb77257958a9cdc5
Louison <louison59279@gmail.com>
2021-11-05 18:21:55 +0100

    tableau de 123 ingredients

--- JS/indexNative.js
+++ JS/indexNative.js
@@ -1,8 +1,20 @@
 import recipes from './recipes.js'
 
+let ingredientsTab = []
+
 for (let i = 0; i < recipes.length; i ++) {
-    console.log(recipes[i].name)
-    for (let j = 0; j < recipes[i].ingredients.length; j++) {
-        console.log(recipes[i].ingredients[j])
-    }
+  for (let j = 0; j < recipes[i].ingredients.length; j++) {
+        ingredientsTab.push(recipes[i].ingredients[j].ingredient)        
+  }
 }
+
+// filtrer le tableau en mettant en minuscule et retirer les doublons
+let tmp = ingredientsTab.join('~').toLowerCase()
+ingredientsTab = tmp.split('~')
+let ingredientTabFiltered = []
+ingredientTabFiltered = new Set(ingredientsTab)
+
+console.log(ingredientTabFiltered)
+
+
+
