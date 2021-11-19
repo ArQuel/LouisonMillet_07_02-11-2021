b1479df894abc66813a0ad317914b63927661796
Louison <louison59279@gmail.com>
2021-11-19 19:35:23 +0100

    marche après tutorat

--- JS/indexNative.js
+++ JS/indexNative.js
@@ -5,7 +5,14 @@ let ustensilsTab = []
 let applianceTab = []
 
 
-// Aller chercher les ingrédients et les mettre dans un tableau
+
+
+main();
+
+
+
+function main (){
+  // Aller chercher les ingrédients et les mettre dans un tableau
 for (let i = 0; i < recipes.length; i ++) {
   for (let j = 0; j < recipes[i].ingredients.length; j++) {
         ingredientsTab.push(recipes[i].ingredients[j].ingredient)        
@@ -21,7 +28,6 @@ for (let i = 0; i < recipes.length; i ++) {
 for (let i = 0; i < recipes.length; i ++) {
         if (recipes[i].ustensils.length > 0 ) {
           for (let j = 0; j < recipes[i].ustensils.length; j++) {
-          let tmp = recipes[i].ustensils[j]
           ustensilsTab.push(recipes[i].ustensils[j])
         }
       } else {
@@ -29,46 +35,72 @@ for (let i = 0; i < recipes.length; i ++) {
       }
 }
 
-let ingredientsTabFinal = formatList(ingredientsTab)
+let iFinal = formatTab(ingredientsTab)
+
+let aFinal = formatTab(applianceTab)
+
+let uFinal = formatTab(ustensilsTab)
+
+const containerIngredients = document.querySelector('.container-ingredients')
+const containerAppareils = document.querySelector('.container-appareil')
+const containerUstensils = document.querySelector('.container-ustensil')
+
+  addEventsTo(containerIngredients, containerAppareils, containerUstensils , iFinal, aFinal, uFinal)
+  // displayCard()
+}
 
 
-let applianceTabFinal = formatList(applianceTab)
+function formatTab(tab){
+  // filtrer le tableau des ustensils en mettant en minuscule et retirer les doublons
+let tmp = tab.join('~').toLowerCase()
+const newTab = tmp.split('~')
+
+return Array.from(new Set(newTab))
+}
 
 
-let ustensilsTabFinal = formatList(ustensilsTab)
+function addEventsTo (containerIngredients, containerAppareils, containerUstensils, iFinal, aFinal, uFinale) {
+    // Afficher les ingrédients dans la liste
+  containerIngredients.addEventListener('click', (e) => {
+    const ListGroupIngredients = document.querySelector('.ingredients-list')
+    displayList(ListGroupIngredients, iFinal, 'ingredients')
+    dropUp(containerIngredients)
+  })
+
+  // Afficher les appareils dans la liste
+  containerAppareils.addEventListener('click', (e) => {
+    const ListGroupAppliance = document.querySelector('.appareil-list')
+    displayList(ListGroupAppliance, aFinal, 'appareils')
+    dropUp(containerAppareils)
+
+  })
+
+  // Afficher les ustensils dans la liste
+  containerUstensils.addEventListener('click', (e) => {
+    const ListGroupUstensils = document.querySelector('.ustensil-list')
+    displayList(ListGroupUstensils, uFinale, 'ustensils')
+    dropUp(containerUstensils)
+  })
+
+}
 
 
 
-// Afficher les ingrédients dans la liste
-const boutonIngredients = document.querySelector('.container-ingredients')
-// Pour la deuxième partie
-// boutonIngredients.addEventListener('click', (e) => {
-//   ListGroupIngredients.innerHTML = ingredientsTabFinal.map(elt =>  `<li class="list-group-item">${elt}</li>` ).join('')
-// })
-boutonIngredients.addEventListener('click', (e) => {
-  // Ajouter des Elts dans le html de ListGroupIngredients déploie les autres containers 
-  const ListGroupIngredients = document.querySelector('.ingredients-list')
-  actualiserTableau(ListGroupIngredients, ingredientsTabFinal)
-})
-
-function actualiserTableau(list, currentTab) {
+function displayList (list, currentTab, tabName) {
   list.innerHTML = ''
-  for (let i = 0; i < 30; i ++) {
-            list.innerHTML += 
-            `<div class="list-group-item ingredients">${currentTab[i][0].toUpperCase() +  
-              currentTab[i].slice(1)}</div>`
-              // Comment faire pour réutiliser cette fonction avec ustensils et appareils ?
-              let items = document.querySelectorAll('.ingredients')
+  for (let i = 0; i < currentTab.length; i ++) {
+            list.innerHTML += `<div class="list-group-item ${tabName}">${currentTab[i][0].toUpperCase() + currentTab[i].slice(1)}</div>`              
+              let items = document.querySelectorAll(`.${tabName}`)
               for (let i = 0; i < items.length; i++) {
                 items[i].addEventListener('click', (e) => {
                   let tagContainer = document.querySelector('.tags')
                   tagContainer.innerHTML += `<div class="tag">
                     <p>${items[i].innerText}</p>
-                    <span class="cross-ingredients">X</span>
+                    <span class="cross-${tabName}">X</span>
                   </div>`
                   // Retirer le tag de la liste
-                  currentTab.splice(i, i);
-                  let cross = document.querySelectorAll('.cross-ingredients')
+                  currentTab.splice(i, 1);
+                  let cross = document.querySelectorAll(`.cross-${tabName}`)
                   for (let j = 0; j < cross.length; j++){
                     cross[j].addEventListener('click', (e) => {
                       let tagElt = cross[j].parentNode
@@ -76,7 +108,7 @@ function actualiserTableau(list, currentTab) {
                       console.log(tagElt)
                       currentTab.push(value)
                       tagElt.parentNode.removeChild(tagElt)
-                      actualiserTableau(list, currentTab, items)
+                      displayList(list, currentTab, tabName)
                     })
                   }
                 })
@@ -84,56 +116,51 @@ function actualiserTableau(list, currentTab) {
   }
 }
 
-// Afficher les appareils dans la liste
-const boutonAppliance = document.querySelector('.container-appareil')
-const ListGroupAppliance = document.querySelector('.appareil-list')
-boutonAppliance.addEventListener('click', (e) => {
-  ListGroupAppliance.innerHTML = ''
-for (let i = 0; i < applianceTabFinal.length; i ++) {
-          ListGroupAppliance.innerHTML += 
-          `<div class="list-group-item appareils">${applianceTabFinal[i][0].toUpperCase() +  
-            applianceTabFinal[i].slice(1)}</div>`
-            let item = document.querySelectorAll('.appareils')
-            for (let i = 0; i < item.length; i++) {
-              item[i].addEventListener('click', (e) => {
-                let tagContainer = document.querySelector('.tags')
-                tagContainer.innerHTML += `<div class="tag"><p>${item[i].innerText}</p><span class="cross-appareils">X</span></div>`
-                // Retirer le tag de la liste
-                applianceTabFinal.splice(i, i);
-                let cross = document.querySelectorAll('.cross-appareils')
 
-              })
-            }
+function getRecipesWithIngredient (recipes, search) {
+  let recipesWithIngredient = []
+  for (let index = 0; index < recipes.length; index ++) {
+    const recipe = recipes[index]
+    for (let i = 0; i < recipe.length; i++) {
+      let ingredient = recipe.ingredients[i].ingredient;
+      if (ingredient === search) {
+        recipesWithIngredient.push(recipe)
+      }
+    }
+  }
+  return [... new Set(recipesWithIngredient)]
 }
-})
 
-// Afficher les ustensils dans la liste
-const boutonUstensils = document.querySelector('.container-ustensil')
-const ListGroupUstensils = document.querySelector('.ustensil-list')
-boutonUstensils.addEventListener('click', (e) => {
-  ListGroupUstensils.innerHTML = ''
-for (let i = 0; i < ustensilsTabFinal.length; i ++) {
-          ListGroupUstensils.innerHTML += 
-          `<div class="list-group-item ustensils">${ustensilsTabFinal[i][0].toUpperCase() +  
-            ustensilsTabFinal[i].slice(1)}</div>`
-            let item = document.querySelectorAll('.ustensils')
-            for (let i = 0; i < item.length; i++) {
-              item[i].addEventListener('click', (e) => {
-                let tagContainer = document.querySelector('.tags')
-                tagContainer.innerHTML += `<div class="tag"><p>${item[i].innerText}</p><span class="cross-ustensils">X</span></div>`
-                // Retirer le tag de la liste
-                ustensilsTabFinal.splice(i, i);
-                let cross = document.querySelectorAll('.cross-ustensils')
-              })
-            }
+function dropUp(ctnElt){
+  document.addEventListener('click', function(event) {
+    let isClickInsideElement = ctnElt.contains(event.target)
+    if (!isClickInsideElement) {
+      console.log('pas dedans')
+      ctnElt.classList.remove("active")
+    }
+    else {
+      console.log('dedans')
+      ctnElt.classList.add("active")
+    }
+})
 }
-})
 
-function formatList(list){
-  // filtrer le tableau des ustensils en mettant en minuscule et retirer les doublons
-let tmp = list.join('~').toLowerCase()
-const newList = tmp.split('~')
-// Pour la deuxième partie
+
+
+
+
+
+//        ---------------------------------------------------------------------- POUR LA PROCHAINE PARTIE --------------------------------------------------------------------------------
+
+
+
+// containerIngredients.addEventListener('click', (e) => {
+//   ListGroupIngredients.innerHTML = ingredientsTabFinal.map(elt =>  `<li class="list-group-item">${elt}</li>` ).join('')
+// })
+
+
+// function formatList(list){
+//   // filtrer le tableau des ustensils en mettant en minuscule et retirer les doublons
 // const newList = list.map(elt => elt.toLowerCase())
-return Array.from(new Set(newList))
-}
+// return Array.from(new Set(newList))
+// }
