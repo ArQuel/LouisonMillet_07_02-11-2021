483e1090aee1356b494b1e0e6f18142e233eb069
Louison <louison59279@gmail.com>
2021-12-01 15:57:11 +0100

    ajout recherche dans sections tags

--- JS/indexNative.js
+++ JS/indexNative.js
@@ -63,12 +63,24 @@ return Array.from(new Set(newTab))
 }
 
 
-function addEventsTo (containerIngredients, containerAppareils, containerUstensils, iFinal, aFinal, uFinale) {
+function addEventsTo (containerIngredients, containerAppareils, containerUstensils, iFinal, aFinal, uFinal) {
     // Afficher les ingrédients dans la liste
   containerIngredients.addEventListener('click', (e) => {
     const ListGroupIngredients = document.querySelector('.ingredients-list')
     displayList(ListGroupIngredients, iFinal, 'ingredients')
-    
+  })
+
+  const inputIngredients = document.querySelector('#Ingredients')
+  inputIngredients.addEventListener('keyup', (e) => {
+    const ListGroupIngredients = document.querySelector('.ingredients-list')
+    let ingredientTabSearch = []
+    for (let i = 0; i < iFinal.length; i++) {
+      console.log(inputIngredients.value, iFinal[i])
+      if ( inputIngredients.value.toLowerCase() === iFinal[i]){
+        ingredientTabSearch.push(iFinal[i])
+        displayList(ListGroupIngredients, ingredientTabSearch, 'ingredients')
+      }
+    }
   })
 
   // Afficher les appareils dans la liste
@@ -76,14 +88,39 @@ function addEventsTo (containerIngredients, containerAppareils, containerUstensi
     const ListGroupAppliance = document.querySelector('.appareil-list')
     displayList(ListGroupAppliance, aFinal, 'appareils')
     dropUp(containerAppareils)
+  })
 
+  const inputAppareils = document.querySelector('#Appareils')
+  inputAppareils.addEventListener('keyup', (e) => {
+    const ListGroupAppliance = document.querySelector('.appareil-list')
+    let appareilTabSearch = []
+    for (let i = 0; i < aFinal.length; i++) {
+      console.log(inputAppareils.value, aFinal[i])
+      if ( inputAppareils.value.toLowerCase() === aFinal[i]){
+        appareilTabSearch.push(aFinal[i])
+        displayList(ListGroupAppliance, appareilTabSearch, 'appareils')
+      }
+    }
   })
 
   // Afficher les ustensils dans la liste
   containerUstensils.addEventListener('click', (e) => {
     const ListGroupUstensils = document.querySelector('.ustensil-list')
-    displayList(ListGroupUstensils, uFinale, 'ustensils')
+    displayList(ListGroupUstensils, uFinal, 'ustensils')
     dropUp(containerUstensils)
+  })
+
+  const inputUstensils = document.querySelector('#Ustensils')
+  inputUstensils.addEventListener('keyup', (e) => {
+    const ListGroupUstensils = document.querySelector('.ustensil-list')
+    let ustensilsTabSearch = []
+    for (let i = 0; i < uFinal.length; i++) {
+      console.log(inputUstensils.value, uFinal[i])
+      if ( inputUstensils.value.toLowerCase() === uFinal[i]){
+        ustensilsTabSearch.push(uFinal[i])
+        displayList(ListGroupUstensils, ustensilsTabSearch, 'ustensils')
+      } 
+    }
   })
 
   dropUp(containerIngredients)
@@ -101,43 +138,55 @@ function displayList (list, currentTab, tabName) {
             list.innerHTML += `<div class="list-group-item ${tabName}">${currentTab[i][0].toUpperCase() + currentTab[i].slice(1)}</div>`              
               let items = document.querySelectorAll(`.${tabName}`)
               for (let i = 0; i < items.length; i++) {
-                items[i].addEventListener('click', (e) => {
-                  let tagContainer = document.querySelector('.tags')
-                  tagContainer.innerHTML += `<div class="tag tag-${tabName}" data-index="${selectedTags.length}">
-                    <p>${items[i].innerText}</p>
-                    <span class="cross-${tabName}">X</span>
-                  </div>`
-                  selectedTags.push(items[i].innerText)
-                  // Retirer le tag de la liste
-                  currentTab.splice(i, 1);
-                  let cross = tagContainer.querySelectorAll('span')
-                  for (let index = 0; index < cross.length; index++){
-                    cross[index].addEventListener('click', (e) => {
-                      let tagElt = cross[index].parentNode
-                      console.log(tagElt)
-                      let value = tagElt.innerText
-                      currentTab.push(value)
-                      tagElt.parentNode.removeChild(tagElt)
-                      selectedTags.splice(e.target.closest('.tag').dataset.index, 1)
-                      displayList(list, currentTab, tabName)
-                    })
-                  }
-                   let search = items[i].innerText
-                   let filtredIngredients, filtredAppliances, filtredUstensils
-                   // Comment faire les troix en même temps ?
-                   filtredIngredients = [...getRecipesWithIngredient(filtredRecipes, search)]
-                   console.log(filtredIngredients)
-                   filtredAppliances = [... getRecipesWithAppliance(filtredRecipes, search)]
-                   console.log(filtredAppliances)
-                   filtredUstensils = [... getRecipesWithUstensil(filtredRecipes, search)]
-                   console.log(filtredUstensils)
-                   filtredRecipes = filtredIngredients.concat(filtredAppliances.concat(filtredUstensils))
-                  displayCards(filtredRecipes)
-                })
+                getTags(items, i, tabName, currentTab, list);
               }
   }
 }
 
+
+function getTags(items, i, tabName, currentTab, list) {
+  items[i].addEventListener('click', (e) => {
+    let tagContainer = document.querySelector('.tags');
+    tagContainer.innerHTML += `<div class="tag tag-${tabName}" data-index="${selectedTags.length}">
+                    <p>${items[i].innerText}</p>
+                    <span class="cross-${tabName}">X</span>
+                  </div>`;
+    selectedTags.push(items[i].innerText);
+    // Retirer le tag de la liste
+    currentTab.splice(i, 1);
+    addCross(tagContainer, currentTab, list, tabName);
+    refreshRecipes(items, i);
+  });
+}
+
+function refreshRecipes(items, i) {
+  let search = items[i].innerText;
+  let filtredIngredients, filtredAppliances, filtredUstensils;
+  // Comment faire les troix en même temps ?
+  filtredIngredients = [...getRecipesWithIngredient(filtredRecipes, search)];
+  console.log(filtredIngredients);
+  filtredAppliances = [...getRecipesWithAppliance(filtredRecipes, search)];
+  console.log(filtredAppliances);
+  filtredUstensils = [...getRecipesWithUstensil(filtredRecipes, search)];
+  console.log(filtredUstensils);
+  filtredRecipes = filtredIngredients.concat(filtredAppliances.concat(filtredUstensils));
+  displayCards(filtredRecipes);
+}
+
+function addCross(tagContainer, currentTab, list, tabName) {
+  let cross = tagContainer.querySelectorAll('span');
+  for (let index = 0; index < cross.length; index++) {
+    cross[index].addEventListener('click', (e) => {
+      let tagElt = cross[index].parentNode;
+      console.log(tagElt);
+      let value = tagElt.innerText;
+      currentTab.push(value);
+      tagElt.parentNode.removeChild(tagElt);
+      selectedTags.splice(e.target.closest('.tag').dataset.index, 1);
+      displayList(list, currentTab, tabName);
+    });
+  }
+}
 
 function getRecipesWithIngredient (recipes, search) {
   let recipesWithIngredient = []
